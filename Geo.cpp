6083dd7b459834f807ffba37d9244c3320bc7453
K2ot <krzysztof.e.kot@gmail.com>
2023-01-08 21:59:40 +0100

    Finalnie

--- Geo.cpp
+++ Geo.cpp
@@ -1,46 +1,47 @@
 #include "Geo.h"
 
+const std::string HOST = "localhost";
+const std::string USER = "postgres";
+const std::string PASSWORD = "1234";
+const std::string DBNAME = "polandDB_xyz";
+
 int main()
 {
-	
 	setlocale(LC_CTYPE, "Polish");
 	std::mt19937 generator(time(0));
 
-	// £¹czenie z baz¹ danych
-	PGconn* conn = PQconnectdb("host=localhost user=postgres password=1234 dbname=polandDB_xyz");
+	std::stringstream connectionString;
+	connectionString << "host=" << HOST << " user = " << USER << " password = " << PASSWORD << " dbname = " << DBNAME;
+	std::string conne = connectionString.str();
+	PGconn* conn = PQconnectdb(conne.c_str());
 
-	// Sprawdzanie statusu po³¹czenia
 	if (PQstatus(conn) != CONNECTION_OK)
 	{
 		std::cerr << "B³¹d po³¹czenia z baz¹ danych: " << PQerrorMessage(conn) << std::endl;
 		return 1;
 	}
-	
-	cleanDB(conn);
 
-	
-	int i = 0, x = 0, y = 0, z = 0;
+	//cleanDB(conn);
+
+	int i = 0;
 	while (i < NO_OF_POINTS)
-	{
+	{ 
+		int x = 0, y = 0, z = 0;
 		x = RandomNum(generator, MIN_X, MAX_X);
 		y = RandomNum(generator, MIN_Y, MAX_Y);
 		z = RandomNum(generator, MIN_Z, MAX_Z);
 
 		if (isPointInPL(conn, x, y) == true)
 		{
-
 			if (!testDistance(conn, x, y))
 			{
 				continue;
 			}
-
 			insertPoint(conn, x, y, z);
 			i++;
 		}
 	}
-
-	showPointByWoj(conn);
-
+	showPointsByWoj(conn);
 	PQfinish(conn);
 	return 0;
 }
--- Geo.h
+++ Geo.h
@@ -1,12 +1,11 @@
 #pragma once
-
 #include <sstream>
 #include <iostream>
 #include <libpq-fe.h>
 #include <random>
-#include <iomanip>
+#include <vector>
 
-const int NO_OF_POINTS = 100;
+const int NO_OF_POINTS = 2000;
 const int MIN_DISTANCE = 3000;
 const int MIN_X = 130000;
 const int MAX_X = 790000;
@@ -15,22 +14,41 @@ const int MAX_Y = 862000;
 const int MIN_Z = 0;
 const int MAX_Z = 300;
 
+class Woj
+{
+	int id_;
+	std::string name_;
+
+public:
+
+	Woj(int id, std::string name)
+	{
+		id_ = id;
+		name_ = name;
+	}
+	~Woj() = default;
+
+	int getId()
+	{
+		return id_;
+	}
+
+	std::string getName()
+	{
+		return name_;
+	}
+};
+
 bool isPointInPL(PGconn* conn, int x, int y)
 {
 	std::stringstream ss;
-	ss << "SELECT st_contains(geom, ST_GeomFromText('POINT(" << x << " " << y << ")', 4326)) FROM redlining;"; //test czy jest w obrysie
+	ss << "SELECT ST_Contains(geom, ST_GeomFromText('POINT(" << x << " " << y << ")', 4326)) FROM redlining;";
 	std::string query = ss.str();
 	PGresult* res = PQexec(conn, query.c_str());
-
-	if (PQresultStatus(res) != PGRES_TUPLES_OK)
-	{
-		std::cerr << "B³¹d zapytania: " << PQerrorMessage(conn) << std::endl;
-		PQclear(res);
-		return false;
-	}
-	std::string val = PQgetvalue(res, 0, 0);
+	std::string value = PQgetvalue(res, 0, 0);
 	PQclear(res);
-	return (val == "t");
+
+	return (value == "t");
 }
 
 int RandomNum(std::mt19937& generator, int min, int max)
@@ -39,65 +57,70 @@ int RandomNum(std::mt19937& generator, int min, int max)
 	return distribution(generator);
 }
 
-bool testDistance(PGconn* conn, int x, int y)
+bool testDistance(PGconn* conn, int  x, int  y) 
 {
-	std::stringstream ssTest;
-	ssTest << "SELECT * FROM (SELECT ST_Distance(ST_MakePoint(" << x << ", " << y << "), ST_MakePoint(x, y)) FROM punkty_w_pl) as q where st_distance < " << MIN_DISTANCE << ";";
-	std::string query = ssTest.str();
-	PGresult* resTest = PQexec(conn, query.c_str());
-	int ntuples = PQntuples(resTest);
-	PQclear(resTest);
+	std::stringstream ss; 
+	ss << "SELECT * FROM (SELECT ST_Distance(ST_MakePoint(" << x << ", " << y << "), ST_MakePoint(x, y)) FROM punkty_w_pl) AS q WHERE ST_Distance < " << MIN_DISTANCE << ";";
+	std::string query = ss.str();
+	PGresult* res = PQexec(conn, query.c_str()); 
+	int ntuples = PQntuples(res);
+	PQclear(res);
 	return ntuples == 0;
 }
 
-void insertPoint(PGconn* conn, int x, int y, int z)
+void insertPoint(PGconn* conn, int x, int y, int z) 
 {
-	std::stringstream ssIn;
-	ssIn << "INSERT INTO punkty_w_pl (x, y, z) VALUES (" << x << " , " << y << " , " << z << ");";
+	std::stringstream ss; 
+	ss << "INSERT INTO punkty_w_pl (x, y, z) VALUES (" << x << " , " << y << " , " << z << ");";
 	
-	std::string query = ssIn.str();
-	PGresult* resIn = PQexec(conn, query.c_str());
+	std::string query = ss.str();
+	PGresult* res = PQexec(conn, query.c_str()); 
 
-	if (PQresultStatus(resIn) != PGRES_COMMAND_OK) {
-		// W razie b³êdu wyœwietlenie informacji o b³êdzie
-		std::cerr << "B³¹d wykonania polecenia INSERT: " << PQerrorMessage(conn) << std::endl;
-	}
-	PQclear(resIn);
+	PQclear(res);
 }
 
-void showPointByWoj(PGconn* conn)
+std::vector<Woj> getWoj(PGconn* conn)
 {
-	std::stringstream ssSh;
-	ssSh << " SELECT gid, jpt_nazwa_ FROM redlining_woj;";
+	std::stringstream ss;
+	ss << " SELECT gid, jpt_nazwa_ FROM redlining_woj; ";
+	std::string query = ss.str();
+	PGresult* res = PQexec(conn, query.c_str());
+	int nWoj = PQntuples(res);
 
-	std::string query = ssSh.str();
-	PGresult* resSh = PQexec(conn, query.c_str());
-
-	//if (PQresultStatus(resSh) != PGRES_COMMAND_OK) {
-	//	// W razie b³êdu wyœwietlenie informacji o b³êdzie
-	//	std::cerr << "B³¹d wykonania polecenia" << PQerrorMessage(conn) << std::endl;
-	//}
-
-	int nWoj = PQntuples(resSh);
+	std::vector<Woj> wojs = {};
 	for (size_t i = 0; i < nWoj; i++)
 	{
-		std::cout << std::endl << PQgetvalue(resSh, i, 1) << std::endl;
-
-		std::stringstream ssSh2;
-		ssSh2 << "SELECT x, y ,z FROM punkty_w_pl as punkty, redlining_woj as woj WHERE ";
-		ssSh2 << "woj.gid = "<< PQgetvalue(resSh, i, 0 )<<" AND st_contains(woj.geom, ST_GeomFromText('POINT(' || punkty.x || ' ' || punkty.y || ')', 4326)); ";
-
-		std::string query2 = ssSh2.str();		
-		PGresult* resSh2 = PQexec(conn, query2.c_str());
-		int nPoint = PQntuples(resSh2);
-		for(size_t j = 0; j < nPoint; j++)
-		{
-			std::cout << PQgetvalue(resSh2, j, 0) << " " << PQgetvalue(resSh2, j, 1) << " " << PQgetvalue(resSh2, j, 2) << std::endl;
-		}
-		PQclear(resSh2);
+		wojs.push_back(Woj(atoi(PQgetvalue(res, i, 0)), PQgetvalue(res, i, 1)));
 	}
 
-	PQclear(resSh);
+	PQclear(res);
+	return wojs;
+}
+
+void printPoints(PGconn* conn, Woj & woj)
+{
+	std::stringstream ss;
+	ss << " SELECT x, y, z FROM punkty_w_pl as punkty, redlining_woj AS woj WHERE ";
+	ss << " woj.gid = " << woj.getId() << " AND ST_Contains(woj.geom, ST_GeomFromText('POINT(' || punkty.x || ' ' || punkty.y || ')', 4326)); ";
+	std::string query = ss.str();
+	PGresult* res = PQexec(conn, query.c_str());
+	int nPoint = PQntuples(res);
+	std::cout << "     x      y   z [m] " << std::endl;
+	for (size_t j = 0; j < nPoint; j++)
+	{
+		std::cout << PQgetvalue(res, j, 0) << " " << PQgetvalue(res, j, 1) << " " << PQgetvalue(res, j, 2) << std::endl;
+	}
+	PQclear(res);
+}
+
+void showPointsByWoj(PGconn* conn)
+{
+	std::vector<Woj> wojs = getWoj(conn);
+	for (size_t i = 0; i < wojs.size(); i++)
+	{
+		std::cout << std::endl << wojs[i].getName() << std::endl;
+		printPoints(conn, wojs[i]);
+	}
 }
 
 void cleanDB(PGconn* conn)
@@ -108,4 +131,3 @@ void cleanDB(PGconn* conn)
 	PGresult* resCln = PQexec(conn, query.c_str());
 	PQclear(resCln);
 }
-
